db1855332cd08d8cf24bed269ce973c3db203b7a
Laura Mendieta <laumendieta06@gmail.com>
2024-06-18 18:12:56 -0500

    [CREATE] RETO 1

---
+++ RETO1/scripts/index.js
@@ -0,0 +1,32 @@
+// SIEMPRE PONER DEFER RN EL HTMLNO OLVIDAR
+// Leer el evento del formulario
+document.querySelector(".formularioNombres").addEventListener('submit', (e) =>{
+    // Evita que aparezca el codigo de envio en pantalla al enviar
+    e.preventDefault();
+
+    // Obtener el valor de los inputs en un arreglo
+    const nombresExtraidos = document.getElementsByClassName("nombre");
+    // Definir el arreglo donde se guardaran
+    // El Set asegura que no hayan duplicados
+    const nombres = new Set();
+
+    for(let i=0; i<nombresExtraidos.length;i++){
+        // Extrae el valor que se encuentre en esa posicion del arreglo
+        // El .trim() Elimina espacios en blanco
+        const nombre = nombresExtraidos[i].value.trim();
+        // Validar que no este en blanco
+        if(nombre){
+            nombres.add(nombre);
+        }
+    }
+
+    // Obtener el lugar donde se mostraran resultados
+    const nombresFinales = document.querySelector(".nombresResultado");
+    // Limpiar el contenido que este
+    nombresFinales.innerHTML = "";
+
+    nombres.forEach(nombre =>{
+        // Añade el elemento al html
+        nombresFinales.innerHTML += `<p>${nombre}</p>`;
+    })
+})
